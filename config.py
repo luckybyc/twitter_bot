1c4589c7adea840a61720f0b6c8f253424463a09
luckybyc <35312301+luckybyc@users.noreply.github.com>
2022-01-25 21:34:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
-# Edit this confif file as you like
 
-# This is hastag which Twitter bot will search and retweet. You can edit this with any hastag .For example : '#javascript'
 
 QUERY = '#python'
 
