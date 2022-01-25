484711751b9094cba1502f119fc2ec03a56e8bc5
luckybyc <35312301+luckybyc@users.noreply.github.com>
2022-01-25 21:34:04 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+
 
 consumer_key = 'your_consumer_key'
 consumer_secret = 'your_consumer_secret'
